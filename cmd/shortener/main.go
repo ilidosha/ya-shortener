b9acbc73cfe6d947b9b6847d39a6f4148a5362d3
ilidosha <axesaspw10@gmail.com>
2024-05-29 17:16:47 +0500

    change timing

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -80,7 +80,7 @@ func main() {
 			}
 
 			// Run the hard delete function every 30 seconds
-			ticker := time.NewTicker(200 * time.Millisecond)
+			ticker := time.NewTicker(20 * time.Second)
 			defer ticker.Stop()
 			for range ticker.C {
 				actualDeletingFunction()
